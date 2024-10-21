93f84961e8b48a5570679803a232ac46abd7e1bc
EllaNillnor <elni06001@utb.vaxjo.se>
2024-10-21 09:38:10 +0200

    array uppgift 2 med många decimaler

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+double[] temperatur={4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9};
+	Console.WriteLine("Medelvärde: "+ Medel(temperatur));
+
+double Medel(double[] v){
+    double summa = 0; 
+    int antal = 0;
+    for(int i=0; i<7; i++){
+        summa += v[i];
+        antal++;
+    }
+    return(summa/antal);
+}
